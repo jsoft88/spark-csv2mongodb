105748a5a999f9fa5bf80e115a4bb62de6733836
jsoft88 <jorgitouni@gmail.com>
2021-02-16 20:56:36 +0100

    * trying to fix curl requests to livy container

--- src/test/resources/integration_test/run_spark_job.sh
+++ src/test/resources/integration_test/run_spark_job.sh
@@ -22,4 +22,4 @@ RESPONSE=$(curl -X POST http://localhost:8998/batches \
 EOF
 )
 
-echo "$RESPONSE" #| tr -d '{'| tr -d '}' | tr ',' '\n' | grep -w id | awk -F ':' '{print $NF}' | tr -d '"'
+echo "$RESPONSE" | tr -d '{'| tr -d '}' | tr ',' '\n' | grep -w id | awk -F ':' '{print $NF}' | tr -d '"'

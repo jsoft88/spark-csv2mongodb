f116d978f0dcdd5e9023c1abe0d24b7da9e07cfd
jsoft88 <jorgitouni@gmail.com>
2021-02-16 20:22:24 +0100

    * trying to fix curl requests to livy container

--- src/test/resources/integration_test/spark_job_polling.sh
+++ src/test/resources/integration_test/spark_job_polling.sh
@@ -5,11 +5,11 @@ set -e
 [ -z "$1" ] && exit 1
 
 BATCH_ID=$1
-JOB_STATUS=$(curl -X GET localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
+JOB_STATUS=$(curl -X GET http://127.0.0.1:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
 while [ "$JOB_STATUS" = "running" ];
 do
   sleep 10
-  JOB_STATUS=$(curl -X GET localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
+  JOB_STATUS=$(curl -X GET http://127.0.0.1:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
 done
 
 echo "$JOB_STATUS"

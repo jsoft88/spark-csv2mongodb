b8056bc73083b9394fae6aa1b3a9fb178e41eff8
jsoft88 <jorgitouni@gmail.com>
2021-02-16 16:36:18 +0100

    * fixed parsing of json responses from livy

--- src/test/resources/integration_test/spark_job_polling.sh
+++ src/test/resources/integration_test/spark_job_polling.sh
@@ -5,11 +5,11 @@ set -e
 [ -z "$1" ] && exit 1
 
 BATCH_ID=$1
-JOB_STATUS=$(curl -X GET http://localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | awk -F ',' '{print $NF}' | awk -F ':' '{print $NF}')
-while [ "$JOB_STATUS" = "\"running\"" ];
+JOB_STATUS=$(curl -X GET localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
+while [ "$JOB_STATUS" = "running" ];
 do
   sleep 10
-  JOB_STATUS=$(curl -X GET localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | awk -F ',' '{print $NF}' | awk -F ':' '{print $NF}')
+  JOB_STATUS=$(curl -X GET localhost:8998/batches/"$BATCH_ID"/state | tr -d '{' | tr -d '}' | tr ',' '\n' | grep -w 'state' | awk -F ':' '{print $NF}' | tr -d '"')
 done
 
 echo "$JOB_STATUS"
